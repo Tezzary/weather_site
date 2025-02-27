b0c9365c8cb0bafc0fa24ff9902b8a74c0532402
tezzy <zacharyeverett.00@gmail.com>
2025-02-28 00:47:14 +1100

    started work using tokio on backend rust downloading the weather images from bom, struggling through reqwest library currently

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
+
+use reqwest::{self, header::USER_AGENT};
+use tokio;
+
+const REQUEST_URL: &str = "http://www.bom.gov.au/radar/IDR023.T.202502271249.png";
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    
+    let result = get_req().await;
+    
+}
+
+async fn get_req() -> Result<(),  Box<dyn std::error::Error>> {
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0".parse().unwrap());
+    let client = reqwest::Client::builder()
+    .default_headers(headers)
+    .build()?;
+
+    let response = client.get(REQUEST_URL)
+    .send()
+    .await?
+    .text()
+    .await?;
+
+
+    println!("body = {:?}", response);
+    Ok(())
 }
