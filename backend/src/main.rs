86aa76c7038bea48e5ff86c7d690a61c1ad04efc
tezzy <zacharyeverett.00@gmail.com>
2025-03-01 01:05:45 +1100

    working download and save to disk of singular weather image, reqwest with user agent header now working.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,10 @@
 
 use reqwest::{self, header::USER_AGENT};
 use tokio;
+use std::fs::File;
+use std::io::prelude::*;
 
-const REQUEST_URL: &str = "http://www.bom.gov.au/radar/IDR023.T.202502271249.png";
+const REQUEST_URL: &str = "http://www.bom.gov.au/radar/IDR023.T.202502281319.png";
 
 #[tokio::main]
 async fn main() {
@@ -14,18 +16,19 @@ async fn main() {
 
 async fn get_req() -> Result<(),  Box<dyn std::error::Error>> {
     let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0".parse().unwrap());
+    headers.insert(USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0".parse().unwrap());
     let client = reqwest::Client::builder()
     .default_headers(headers)
     .build()?;
 
-    let response = client.get(REQUEST_URL)
+    let image_bytes = client.get(REQUEST_URL)
     .send()
     .await?
-    .text()
+    .bytes()
     .await?;
 
-
-    println!("body = {:?}", response);
+    let mut file = File::create("test.png")?;
+    let data: Result<Vec<_>, _> = image_bytes.bytes().collect();
+    file.write_all(&data.unwrap())?;
     Ok(())
 }
