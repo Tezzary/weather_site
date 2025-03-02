b0e115caf71c89c4ba07d9a8a11590b555606203
tezzy <zacharyeverett.00@gmail.com>
2025-03-02 23:34:37 +1100

    working on parsing date, date parsing correctly into the same format the site uses, need to now connect that with api requests

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,13 +3,25 @@ use reqwest::{self, header::USER_AGENT};
 use tokio;
 use std::fs::File;
 use std::io::prelude::*;
+use std::time::SystemTime;
+use chrono::{naive, DateTime};
 
-const REQUEST_URL: &str = "http://www.bom.gov.au/radar/IDR023.T.202502281319.png";
+
+const REQUEST_URL: &str = "http://www.bom.gov.au/radar/IDR023.T.202503021144.png";
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    
+    let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+    let date = chrono::DateTime::from_timestamp(time as i64, 0).unwrap();
+
+    let naive_date = date.date_naive();
+    let string_date = naive_date.format("%Y%m%d").to_string();
+    let naive_time = date.time();
+    let string_time = naive_time.format("%H%M").to_string();
+    let combined_string = string_date + &string_time;
+    println!("{}", combined_string);
+
     let result = get_req().await;
     
 }
